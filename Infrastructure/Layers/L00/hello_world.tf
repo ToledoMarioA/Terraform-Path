67935f18c1fdf816cbe500f3296bfe1ecfba6f8a
Ivan-Axity <ivan.ramos@axity.com>
2023-11-23 14:33:36 -0600

    Se agrega hello world para crear grupo de recursos
    Se agrega hello world para crear grupo de recursos

--- Infrastructure/Layers/L00/hello_world.tf
+++ Infrastructure/Layers/L00/hello_world.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.59.0"
+    }
+  required_version = ">= 1.1.0"
+}
+
+}
+provider "azurerm" {
+  
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
+  features {
+  }
+  skip_provider_registration = true
+}
+
 # Crea un grupo de recursos con etiquetas
 resource "azurerm_resource_group" "hello_world_resource_group" {
   name     = "Resources_OXXO_IaC_01"
