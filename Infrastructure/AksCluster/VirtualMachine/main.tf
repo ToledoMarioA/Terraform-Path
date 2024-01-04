4f4237a98710fd2c8466edf521b95d707187c547
Ivan-Axity <ivan.ramos@axity.com>
2024-01-03 22:54:09 -0600

    Se agregan folders de infraestructura para Aks y OpenShift
    Se agregan folders de infraestructura para Aks y OpenShift

---
+++ Infrastructure/AksCluster/VirtualMachine/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.59.0"
+    }
+    azapi = {
+        source  = "Azure/azapi"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  subscription_id = ""
+  features {
+  }
+  skip_provider_registration = true
+}
---
+++ Infrastructure/OpenShiftCluster/VirtualMachine/Readme.md
@@ -0,0 +1,5 @@
+# Propósito del directorio Módules:
+
+Todo GRUPO de recursos utilizados para un propósito en común, deben de ser
+condensados en módulos reutilizables por otras capas del proyecto
+
